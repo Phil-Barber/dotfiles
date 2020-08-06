2a6d05f1af523be1ac3e25d961b2eb9264bf6b3c
Phil Barber <phil@lantum.com>
2020-08-06 16:10:47 +0100

    update tmux

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,8 +1,8 @@
-tmux new-session -s bart -c ~/repos/bart -d
-tmux new-session -s react -c ~/repos/react-web-app -d
-tmux new-session -s native -c ~/repos/lantum-native -d
+tmux new-session -s bart -c ~/repos/lantum/bart -d
+tmux new-session -s react -c ~/repos/lantum/react-web-app -d
+tmux new-session -s native -c ~/repos/lantum/lantum-native -d
 tmux new-session -s todo -c ~/ -d
 tmux send -t todo.0 vim .todo.txt ENTER
-tmux new-session -s stack -c ~/repos/stack -d
+tmux new-session -s stack -c ~/repos/lantum/stack -d
 tmux new-session -s phil -c ~/repos/phil-barber -d
 tmux new-session -s riff -c ~/repos/riff -d
